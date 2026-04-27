1f781f8dab7be81080f5847cefc17008cc5e7e51
asmita <asmi@gmail.com>
2026-04-28 01:45:27 +0530

    fixed render port issue

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,8 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/memory-game");
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 const Score = mongoose.model("Score", { score: Number });
 
 app.get("/highscore", async(req, res) => {
@@ -64,4 +66,8 @@ app.get("/user/:id", async(req, res) => {
     res.json({ username: user.username, highScore: user.highScore });
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
