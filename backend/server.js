49de5cdbe8b4b7b57329c0dbc10fdd65ba50165a
asmita <asmi@gmail.com>
2026-04-28 02:57:18 +0530

    added root route

--- backend/server.js
+++ backend/server.js
@@ -71,3 +71,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
